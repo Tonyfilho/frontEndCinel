4c4e48be083e5b5c989464a924181c9dd64e98d9
Tonyfilho <oficinadosdrones@gmail.com>
2026-05-26 21:37:52 +0100

    main.html e header.html  prontos

---
+++ aula04/src/app/formulario-statico/formulario-statico.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario-statico',
+  imports: [],
+  templateUrl: './formulario-statico.html',
+  styleUrl: './formulario-statico.css',
+})
+export class FormularioStatico {
+
+
+
+
+
+
+
+
+  
+}
