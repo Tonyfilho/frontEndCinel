6ce65b36116b49174d9828c2b510df0981e4fb50
Tonyfilho <oficinadosdrones@gmail.com>
2026-05-26 22:31:58 +0100

    avatar.html feita

--- aula05/src/app/components/avatar/avatar.ts
+++ aula05/src/app/components/avatar/avatar.ts
@@ -6,4 +6,13 @@ import { Component } from '@angular/core';
   templateUrl: './avatar.html',
   styleUrl: './avatar.css',
 })
-export class Avatar {}
+export class Avatar {
+  nome: string = 'Tony';
+  //  nome2:string = `20`;
+  //  nome3:string = 'true';
+
+  logout() {
+    // throw new Error('Method not implemented.');
+    console.log('deslogou');
+  }
+}
