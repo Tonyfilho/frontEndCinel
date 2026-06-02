f8c92529e1c27c094c11a50bbb2a2feb4620fec8
Tonyfilho <oficinadosdrones@gmail.com>
2026-06-02 19:45:53 +0100

    aula06

--- aula06/src/main.ts
+++ aula06/src/main.ts
@@ -1,9 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
-import { Settings } from 'luxon';
 
-
-Settings.defaultLocale = 'pt-PT';
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
