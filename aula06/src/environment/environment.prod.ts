0dc10567d6ccb7cd5952978efb44852ea0ae3e53
Tonyfilho <oficinadosdrones@gmail.com>
2026-06-02 21:43:46 +0100

    environment do projeto

---
+++ aula06/src/environment/environment.prod.ts
@@ -0,0 +1,13 @@
+import { keys } from './environment.keys';
+
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: keys.apiKey,
+    authDomain: 'frontend-cinel-tony.firebaseapp.com',
+    projectId: 'frontend-cinel-tony',
+    storageBucket: 'frontend-cinel-tony.firebasestorage.app',
+    messagingSenderId: '308798349718',
+    appId: '1:308798349718:web:2dabbbcfc36f75df3bfdb7',
+  },
+};
