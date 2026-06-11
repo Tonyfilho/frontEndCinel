79c29fe2352d674ebccaebbd8a6a66618acb7592
Tonyfilho <oficinadosdrones@gmail.com>
2026-06-11 20:03:50 +0100

    fim do tipo de dados

--- aula07/src/app/pages/tipo-de-dados/tipo-de-dados.ts
+++ aula07/src/app/pages/tipo-de-dados/tipo-de-dados.ts
@@ -2,6 +2,8 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
 
+type meuObjeto = { nome: string; endereco: string; numero: number; admin: boolean };
+
 @Component({
   selector: 'app-tipo-de-dados',
   imports: [CommonModule],
@@ -18,28 +20,50 @@ export class TipoDeDados implements OnInit {
   localNull2!: null;
   localUndefined: undefined = undefined;
   localUndefined2?: undefined;
+  localObjecto = { nome: 'tony', endereco: 'rua....', numero: 10, admin: false, lado: 'esquerdo' };
+  localObjecto2: meuObjeto = { nome: 'tony', endereco: 'rua....', numero: 10, admin: false };
 
   /**tipo de dados assyncronos */
-  localPromise: Promise<string> = new Promise<string>((resolve) => { resolve("Aula de Assincronos em Promise")});
-  localObservable$ = new Observable((a) => a.next('Aula de Assincronos Observables'));
+  localPromise: Promise<string> = new Promise<string>((resolve, reject) => {
+    if (resolve) {
+      resolve('Aula de Assincronos em Promise');
+    }
+    reject('Error Http 400');
+  });
+
+  localObservable$ = new Observable((ana) => ana.next('Aula de Assincronos Observables'));
   localObservable2$: Observable<string> = of('Aula de Assincronos Observables2');
 
   /**construtor do JS */
   constructor() {
+    this.localPromise.then((result) =>
+      console.log('********minha variavel de promise com Them ', result),
+    );
     console.log('minha variavel de string:  ', this.localString);
     console.log('minha variavel de number: ', this.localNumber);
     console.log('minha variavel de boolean: ', this.localBollean);
     console.log('minha variavel de array: ', this.localArray);
     console.log('minha variavel de null: ', this.localNull);
     console.log('minha variavel de undefined: ', this.localUndefined);
-    // console.log("minha variavel de promise sem Pending: " , this.localPromise);
-    // console.log("minha variavel de observable sem Subscribe: " , this.localObservable$);
+    // console.log('minha variavel de promise sem Pending: ', this.localPromise);
+    // console.log('minha variavel de observable sem Subscribe: ', this.localObservable$);
+    console.log('minha variavel do tipo Objeto: ', this.localObjecto2);
+    console.log('minha variavel do tipo Objeto.nome: ', this.localObjecto2.nome);
+    console.log('minha variavel do tipo Objeto.endereco: ', this.localObjecto2['endereco']);
+    console.log('minha variavel do tipo Objeto.numero: ', this.localObjecto2.numero);
+    console.log('minha variavel do tipo Objeto.admin: ', this.localObjecto2['admin']);
+
   }
 
   ngOnInit(): void {
     this.localObservable$.subscribe((data) =>
       console.log('minha variavel de observable com Subscribe', data as string),
     );
-    this.localPromise.then((result) => console.log('********minha variavel de promise com Them ', result));
+
+    this.localObservable2$.subscribe({
+      next: (result) => console.log('Nosso 2º Observable: ' + result),
+      error: (e) => console.error('Error do Observable' + e),
+      complete: () => console.log('Observable completo'),
+    });
   }
 }
