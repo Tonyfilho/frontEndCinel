eb45d10a57f644167f7927679bfbf23ecd1d4ba0
Tonyfilho <oficinadosdrones@gmail.com>
2026-07-15 21:59:59 +0100

    formulario funcionando

--- aula09/src/app/app.ts
+++ aula09/src/app/app.ts
@@ -1,15 +1,25 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header } from "./components/header/header";
-import { Main } from "./components/main/main";
-import { Footer } from "./components/footer/footer";
+import { Header } from './components/header/header';
+import { Main } from './components/main/main';
+import { Footer } from './components/footer/footer';
+import { AuthStateService } from './service/auth-state';
 
 @Component({
   selector: 'app-root',
-  imports: [ Header, Main, Footer],
+  imports: [Header, Main, Footer],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
-  
+export class App implements OnInit {
+  constructor(private authStateService: AuthStateService) {}
+
+  ngOnInit(): void {
+    this.authStateService.initAuthListener();
+
+    /**Forma de testar se temos dados */
+    this.authStateService.getUser().subscribe((user) => {
+      console.log('dados do google Auth: ', user);
+    });
+  }
 }
--- aula09/src/app/pages/forms/forms.ts
+++ aula09/src/app/pages/forms/forms.ts
@@ -1,9 +1,54 @@
+import { CommonModule, JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  ReactiveFormsModule,
+  UntypedFormBuilder,
+  UntypedFormGroup,
+  Validators,
+} from '@angular/forms';
+import { AuthStateService } from '../../service/auth-state';
+import { Router, RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-forms',
-  imports: [],
+  /* Um novo Modulo o MOdulo para Formarios*/
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
   templateUrl: './forms.html',
   styleUrl: './forms.css',
 })
-export class Forms {}
+export class Forms {
+  /**Variavel para receber criar Formulario e capturar os dados */
+  protected signInForms: UntypedFormGroup;
+
+  constructor(
+    protected authService: AuthStateService,
+    protected router: Router,
+    private fb: UntypedFormBuilder,
+  ) {
+    this.signInForms = this.fb.nonNullable.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(10)]],
+    });
+     /**O controls e a api do form que controla os Validators e tb os dados */
+    console.log("nosso controls: ", this.signInForms.controls);
+  }
+
+  onSubmit() {
+    if(!this.signInForms.valid) {
+      /**Aprendendo a mudar a rota pelo TS */
+      this.router.navigateByUrl("/");
+    }
+  const localFormEmail = this.signInForms.get("email")?.value;
+  const localFormPassword = this.signInForms.get("password")?.value;
+  const localFormsComplete = this.signInForms.value;
+  console.log(`Dados do formulario passados individualmente:  Email: ${localFormEmail} e Password: ${localFormPassword}`);
+  console.log(`Objeto: formulário completo: ${ JSON.stringify(localFormsComplete)}`);
+
+  }
+
+  goBack = () => {
+    this.signInForms.reset();
+    /**Aprendendo a mudar a rota pelo TS */
+    this.router.navigateByUrl("/");
+  }
+} //endclass
