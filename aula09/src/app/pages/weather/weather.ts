3ec04604dab4dbd516bf78ac4fb0e66ee282a22e
Tonyfilho <oficinadosdrones@gmail.com>
2026-09-08 18:32:03 +0100

    fim da uc

--- aula09/src/app/pages/weather/weather.ts
+++ aula09/src/app/pages/weather/weather.ts
@@ -1,9 +1,44 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { WeatherService } from '../../service/weather';
+import { IWeather } from '../../shared/i-weather';
+import { ReactiveFormsModule, UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-weather',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './weather.html',
   styleUrl: './weather.css',
 })
-export class Weather {}
+export class Weather implements OnInit {
+  private weatherService = inject(WeatherService);
+  protected formsWeather: UntypedFormGroup;
+  protected localWeatherSig = signal<IWeather | undefined>(undefined);
+  protected localCountry: string = 'Santana Portugal';
+  private justLetter: RegExp = /^[a-zA-ZÀ-ÿ\s]+$/;
+
+  constructor(private fb: UntypedFormBuilder) {
+    this.formsWeather = fb.group({
+      city: [Validators.required, Validators.pattern(this.justLetter)],
+    });
+  }
+
+  ngOnInit(): void {
+    this.localCountry === undefined ? this.formsWeather.get('city')?.value : this.localCountry;
+    this.weatherService.getWeather2(this.localCountry).subscribe({
+      next: (res) =>  {this.localWeatherSig.set(res), console.log("meu res", res)},
+      error: (e) => console.error('Nosso Error no metodo getWeather2', e),
+      complete: () => console.log('Fim do Observable'),
+    });
+  }
+
+  submit() {
+    this.localCountry = this.formsWeather.get('city')?.value;
+    this.weatherService.getWeather(this.localCountry).subscribe({
+      next: (res) => {
+        (this.localWeatherSig.set(res), console.log('meu res em getWeather', res));
+      },
+      error: (e) => console.error('Nosso Error no metodo getWeather', e),
+    });
+  }
+}
--- aula09/src/app/service/weather.ts
+++ aula09/src/app/service/weather.ts
@@ -1,6 +1,24 @@
 import { Injectable } from '@angular/core';
+import { keys } from '../../environment/environment.keys';
+import { HttpClient } from '@angular/common/http';
+import { IWeather } from '../shared/i-weather';
 
 @Injectable({
   providedIn: 'root',
 })
-export class WeatherService {}
+export class WeatherService {
+  private readonly apiURL = 'https://api.weatherapi.com/v1/current.json';
+  private readonly apiKey = keys.key;
+
+  constructor(private httpClient: HttpClient) {}
+
+  /**Por Propriedade */
+  getWeather = (city: string) => {
+    return this.httpClient.get<IWeather>(`${this.apiURL}?key=${this.apiKey}&q=${city}&aqi=no`);
+  };
+
+  /**Por metodo */
+  getWeather2(city: string) {
+    return this.httpClient.get<any>(`${this.apiURL}?key=${this.apiKey}&q=${city}&aqi=no`);
+  }
+}
